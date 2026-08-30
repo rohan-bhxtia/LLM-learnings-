3a19c047f8ab83d03a5827f6728a2fec73c4bf62
Rohan <rohanbhatia003@gmail.com>
2026-08-30 16:21:53 +0530

    ..

--- day1/main.py
+++ day1/main.py
@@ -3,11 +3,10 @@ from dotenv import load_dotenv
 
 
 from google import genai
-from openai import OpenAI
+
 
 load_dotenv()
 API_key = os.getenv("Gemini_API")
-openai_key = os.getenv("open_ai")
 client = genai.Client(api_key=API_key)
 
 response = client.models.generate_content(
